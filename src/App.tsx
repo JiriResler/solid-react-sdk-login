2ba12dd2879d13b7da478527b90b17bcb9d8d28e
JiriResler <j.resler.29@gmail.com>
2024-08-30 18:17:20 +0200

    add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { useSession } from "@inrupt/solid-ui-react";
-import Home from "./Home";
-import Login from "./Login";
+// import { useSession } from "@inrupt/solid-ui-react";
+// import Home from "./Home";
+// import Login from "./Login";
 
 function App() {
-  const { session } = useSession();
+  // const { session } = useSession();
 
-  if (session.info.isLoggedIn) {
-    return <Home />
-  }
+  // if (session.info.isLoggedIn) {
+  //   return <Home />
+  // }
 
-  return <Login />
+  // return <Login />
+
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<LandingPage />}/>
+      <Route path='/info' element={<InfoPage />} />
+      <Route path='/settings' element={<SettingsPage />} />
+      <Route path='*' element={<h1>No Match Page</h1>} />
+    </Routes>
+    </>
+  )
+}
+
+function LandingPage() {
+  return (
+    <h1>Landing Page</h1>
+  )
+}
+
+function InfoPage() {
+  return (
+    <h1>Info Page</h1>
+  )
+}
+
+function SettingsPage() {
+  return (
+    <h1>Settings Page</h1>
+  )
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { SessionProvider } from "@inrupt/solid-ui-react";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SessionProvider>
-      <App />
-    </SessionProvider>
+    <BrowserRouter>
+      <SessionProvider>
+        <App />
+      </SessionProvider>
+    </BrowserRouter>
   </StrictMode>
 );
