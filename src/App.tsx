f7fbf705f22369ce70ced5484ec99ae3e1220719
JiriResler <j.resler.29@gmail.com>
2024-09-02 14:26:14 +0200

    add routing to Profile and Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { useSession } from "@inrupt/solid-ui-react";
 import Profile from "./Profile";
 import Login from "./Login";
 import RequireAuth from "./RequireAuth";
 
 function App() {
-  const { session } = useSession();
-
-  if (session.info.isLoggedIn) {
-    return <Profile />;
-  }
-
-  return <Login />;
-
   return (
     <>
       <Routes>
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,12 +1,20 @@
 import { LoginButton } from "@inrupt/solid-ui-react";
+import { useSession } from "@inrupt/solid-ui-react";
+import { Navigate } from "react-router-dom";
 
 function Login() {
+  const { session } = useSession();
+
+  if (session.info.isLoggedIn) {
+    return <Navigate to="/profile" replace />;
+  }
+
   return (
     <>
       <h1>Solid Login App</h1>
       <LoginButton
         oidcIssuer={"https://solidcommunity.net/"}
-        redirectUrl={new URL("/", window.location.href).toString()}
+        redirectUrl={"http://localhost:5173/login"}
         onError={console.error}
       />
       <h3>Identity provider: solidcommunity.net</h3>
